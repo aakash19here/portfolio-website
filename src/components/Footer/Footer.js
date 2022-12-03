6cc3bc4fb066a62d63884ddea2237ac891fe275b
akash19here <spacex3nasa@gmail.com>
2022-12-03 11:53:57 +0530

    Added Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+      <FooterWrapper>
+        <LinkList>
+          <LinkColumn>
+            <LinkTitle>Call</LinkTitle>
+            <LinkItem href="tel:111-111-1111">111-111-1111</LinkItem>
+          </LinkColumn>
+          <LinkColumn>
+            <LinkTitle>Email</LinkTitle>
+            <LinkItem href="mailto:spacex3nasa@gmail.com">spacex3nasa@gmail</LinkItem>
+          </LinkColumn>
+        </LinkList>
+        <SocialIconsContainer>
+          <CompanyContainer>
+            <Slogan>Innovating one project at a time</Slogan>
+          </CompanyContainer>
+          <SocialContainer>
+            <SocialIcons href='https://github.com/aakash19here'>
+                <AiFillGithub size="3rem" />
+            </SocialIcons>
+            <SocialIcons href='https://linkedin.com/aakash19here'>
+                <AiFillLinkedin size="3rem" />
+            </SocialIcons>
+            <SocialIcons href='https://instagram.com/aakash19here'>
+                <AiFillInstagram size="3rem" />
+            </SocialIcons>
+          </SocialContainer>
+        </SocialIconsContainer>
+      </FooterWrapper>
   );
 };
 
